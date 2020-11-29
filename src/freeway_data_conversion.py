6c7b901f80c01d794a146ab55d23f89f80d23922
Santiago <santitobon9@gmail.com>
2020-11-28 22:39:09 -0800

    Updated script

--- src/freeway_data_conversion.py
+++ src/freeway_data_conversion.py
@@ -1,45 +1,57 @@
 import csv
-import json
 import pandas as pd 
 
 # read in csv files into dataframes
-df_de = pd.read_csv ("freeway_detectors.csv")
-df_st = pd.read_csv ("freeway_stations.csv")
-df_hw = pd.read_csv ("highways.csv")
+df_de = pd.read_csv ("data/freeway_detectors.csv")
+df_st = pd.read_csv ("data/freeway_stations.csv")
+df_hw = pd.read_csv ("data/highways.csv")
 
 #creates dictionary of highways
 highways = {
-    "3": {"highwayid": "3",
+    3: {"highwayid": "3",
           "shortdirection": "N",
           "direction": "NORTH",
           "highwayname": "I-205"},
-    "4": {"highwayid": "4",
+    4: {"highwayid": "4",
           "shortdirection": "S",
           "direction": "SOUTH",
           "highwayname": "I-205"}
 }
-#create dictionary of stations
+#print(highways[3])
+
 stations = {}
-station = {}
+
 #add to dictionary of stations from stations dataframe
 for ind in df_st.index:
-    #station = {}
-    s_id = df_st.loc[ind, ["stationid"]]
+    station = {}
+    s_id = df_st["stationid"][ind]
     station["stationid"] = s_id
-    station["upstream"] = df_st.loc[ind, ["upstream"]]
-    station["downstream"] = df_st.loc[ind, ["downstream"]]
-    station["stationclass"] = df_st.loc[ind, ["stationclass"]]
-    station["numberlanes"] = df_st.loc[ind, ["numberlanes"]]
-    station["latlon"] = df_st.loc[ind, ["latlon"]]
-    station["length"] = df_st.loc[ind, ["length"]]
+    station["upstream"] = df_st["upstream"][ind]
+    station["downstream"] = df_st["downstream"][ind]
+    station["stationclass"] = df_st["stationclass"][ind]
+    station["numberlanes"] = df_st["numberlanes"][ind]
+    station["latlon"] = df_st["latlon"][ind]
+    station["length"] = df_st["length"][ind]
     stations[s_id] = station
+    #print ("Station id:", s_id)
+#print ("Stations:", stations)
+
+highway_list = list()
+station_list = list()
 
 #adds highway and station dictionaries for each detector
 for ind in df_de.index:
-    h_id = df_de.loc[ind, ["highwayid"]]
-    df_de.loc[ind, ["highway"]] = highways[h_id]
-    s_id = df_de.loc[ind, ["stationid"]]
-    df_de.loc[ind, ["station"]] = stations[s_id]
+    h_id = df_de["highwayid"][ind]
+    #print ("Highway:", highways[h_id])
+    highway_list.append(highways[h_id])
+    s_id = df_de["stationid"][ind]
+    station_list.append(stations[s_id])
+
+#adds new highway and station columns
+df_de["highway"] = highway_list
+df_de["station"] = station_list
+#print(df_de['highway'])
+#print(df_de['station'])
 
 #Save modifed dataframe to csv
 df_de.to_csv("data/freeway_data.csv", index=False)
